79c63945fd7100c0d64a2917a327a7d78b2b4fb6
Chris Edwards <screwtop.github@ripples.dyndns.org>
2014-01-15 14:21:51 +1300

    Whoops, forgot some new files for Find and Frink server functionality!

---
+++ find_panel.tcl
@@ -0,0 +1,41 @@
+# Components and functionality for Find panel
+
+pack [frame .search] -fill x
+pack [label .search.label -text "Find:"] -side left
+pack [entry .search.entry -width 20 -textvariable ::search_term] -side left
+
+# TODO: clear field button, prev/next buttons, found/not found status, maybe count of matches
+# ...
+
+pack [button .search.next -text "Next \u2192" -command [list find $::search_term]] -side left
+
+bind .search.entry <Return> {find $::search_term}
+
+# TODO: when the search text is changed (or perhaps only when it's cleared to start a new search), resume searching from the start of the buffer.
+
+
+# Functionality and key bindings to toggle the search panel
+
+set ::find_panel_enabled true
+
+# TODO: restore to the correct location in the GUI when re-packed!
+
+proc toggle_find_panel {} {
+	if {!$::find_panel_enabled} {
+		# Activate
+		pack .search -fill x
+		set ::find_panel_enabled true
+		focus .search.entry
+	} else {
+		# Deactivate
+		pack forget .search
+		set ::find_panel_enabled false
+		focus .editor.text
+	}
+}
+
+toggle_find_panel
+
+bind . <Control-f> toggle_find_panel
+bind .editor.text <Control-f> {toggle_find_panel; break}
+
---
+++ frinkserver.tcl
@@ -0,0 +1,59 @@
+#!/usr/bin/wish
+
+# A background process for running Frink
+
+wm withdraw .
+tk appname frinkserver
+
+
+
+# Start Frink process (it's Java so it takes like 10 seconds):
+
+package require Expect
+set timeout 3
+spawn java -cp /usr/share/java/frink.jar frink.parser.Frink
+match_max 100000
+expect -re "Frink - Copyright .* Alan Eliasen, eliasen@mindspring.com.\r\n"
+# TODO: communicate with Piaf to indicate when ready?  Ah, but there might be multiple instances, and we wouldn't know what they were called.
+
+
+# Frink echoes back the command, which we can just discard.
+# Frink does not prompt for more input, which could make determining the end of its output rather impossible.  We can't use exp_continue, cos we won't know when to stop.  We can't use a loop.  Hmm.
+# Ah, we can make our own prompt.  Have it print["frink>"] after evaluating our expression.  Then we can use exp_continue until we find the "frink>" prompt.
+# Also, I think Frink/Java does some crazy things with terminal settings - I see lots of weirdo control characters in the output from `autoexpect`.  No idea what to do about those, really.  Also, Frink echoes back the characters as you type them, and ALSO echoes back the entire line again when you execute it.
+# TODO: errors will probably require special handling (they're usually multi-line Java stack-traces).
+# or maybe just return everything and let the user figure it out.
+
+proc frink {code} {
+	set result [list]	;# Will build result as list of lines
+	exp_send -- "println\[$code\]; print\[\"frink>\"\]"
+	expect -re ".*" {}	;# ignore the text we just sent being echoed back by Frink
+	exp_send -- "\r"	;# Tell Frink to evaluate the line
+	expect -re ".*" {}	;# ignore Frink's echo of the entire line we told it to run
+	# [expect] will match the last line of input if it can, ignoring all the previous lines.  Hmm.
+	# NOTE: if there's an error, our "frink>" prompt won't be output.  Will just timeout for now..
+	# TODO: also figure out syncing problem at start of interaction with Frink process.
+	expect -re "\r\n(.*)\nfrink>" {
+		#	puts "RESULT=$result"
+		#	puts "BUFFER=<<$expect_out(1,string)>>"
+			return [regsub -all "\[\r\n\]+" $expect_out(1,string) "\n"]
+	}
+}
+
+# Remote queries can be done using Tk's [send] command (quick and dirty IPC).
+puts "\nfrinkserver running. Send commands from another Tk process like so:"
+puts "send frinkserver {frink {100000 furlongs/fortnight -> km/hour}}"
+# or, indeed:
+# proc frink {expression} {send frinkserver [list frink $expression]}
+# frink gravity
+
+
+# Once running, simple expressions only take a few milliseconds.
+
+# TODO: graceful closey things?
+proc quit {} {
+	exp_close
+	exp_wait
+	exit
+}
+
