371e58a1fcf52f9f69ae265a0fc76b1126c7b70c
Chris Edwards <screwtop.github@ripples.dyndns.org>
2014-01-17 01:35:32 +1300

    Minor but vital improvements to the Tcl console window.

--- console.tcl
+++ console.tcl
@@ -1,17 +1,27 @@
 # A simple command console window.
-# Could imagine wanting file management, text editing in here alone!
+# Could imagine wanting file management, text editing in here alone!  At least a command history.
 # Would be nicer if we could just have tclsh + rlwrap in a terminal like we get when starting it up manually.
 # Should also be possible to show/hide.
 
 toplevel .console
-pack [entry .console.entry -textvar command] -fill x -side top
+pack [entry .console.entry -textvar command -font $::font] -fill x -side top
 bind .console.entry <Key-Return> {execute %W}
+bind .console.entry <Key-Up> {
+	set command [lindex $::console_history end]
+	# TODO: proper navigation of history, not just last command!
+}
 pack [text .console.output -wrap word] -fill both -expand 1
+.console.output configure -font $::font -background black -foreground green
+
+set ::console_history [list]
+
 proc execute {w} {
 	global command
-	.console.output insert end "% $command\n"
+	.console.output insert end "% $command\n"	;# TODO: tag showing it's user input
 	catch {uplevel #0 eval $command} res
 	.console.output insert end $res\n
+	.console.output see end
+	lappend ::console_history $command
 	set command ""
 }
 #eval pack [winfo children .] -fill both -expand 1
@@ -22,4 +32,5 @@ focus .console.entry
 
 wm withdraw .console	;# Hidden by default
 wm protocol .console WM_DELETE_WINDOW {wm withdraw .console}	;# Hide rather than close when "closed".
+
 
