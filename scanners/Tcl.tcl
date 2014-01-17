191a2e5941720d2cb672ab6a70a806dfceb4dbec
Chris Edwards <chris.edwards@otago.ac.nz>
2014-01-17 17:36:59 +1300

    Significant improvements to the lexical highlighting architecture, and improvements to the Tcl scanner.

--- scanners/Tcl.tcl
+++ scanners/Tcl.tcl
@@ -1,64 +1,40 @@
 # Lexical scanner for Tcl/Tk code for Piaf.
 
-# TODO: factor out a generic "tag" procedure that accepts a tag name and a regexp and applies the general routine.
 # TODO: don't call "get_all" for every single thing - just do it once, store it in a variable, and have the various scanners perform their work on that.
 # TODO: longer term, run all this in a background process, so that the GUI will remain responsive no matter how big the file or how complex the language scanner.  This might mean having to stream the editor buffer (line by line? character by character?) - bringing the further complication that the scanner might not ever have an exact copy of the buffer in its curent state (nonatomic).
 
+# It might make sense to import keywords etc. into the dictionary automatically as well when the language scanner is loaded (but how to remove them afterwards? - might have to start afresh and load the dictionary again).
 
 # Will start with something simple: comments
 # In Tcl, comments are implemented using a command named "#".  There's nothing stopping you from changing that, but that's the default/convention.
 # Because it's a command, it means that it only takes effect as a comment if it's at the start of a line (leading tabs and spaces ignored), or follows a [ or ; character.
-
-proc tag_comments {} {
-#	.editor.text tag remove comment 0.0 end
-	# Now a proc:
-	clear_tag comment
-
-	# Explanation of the regexp here:
-	# "(?:^|\n|;)" -> non-capturing group, matching either start of file, linebreak, or semicolon. Basically matches the start of a possible Tcl command.
-	# "\[ \t\]*" -> any whitespace (but not linebreak) (uh, what about CR?)
-	# "(#\[^\n\]*)\n" -> match the actual comment. "#" up until the next LF.
-	# Bah, I don't think non-capturing groups work sensibly with [regexp -all]. :(  No, wait - just use submatch vars - they will contain the relevant indices!  This means not using "-indices".  Or "-all".  Hmm.
-	# Oh, solved: use a list of variables in the foreach to match pairs, and ignore the first one!  don't even need to use non-capturing group for that, then.
-	foreach {ignore index} [regexp -all -indices -inline "(?:^|\n|;)(?:\[ \t\]*)(#\[^\n\]*)\n" [get_all]] {
-	#	puts "$ignore $index"
-		set start [lindex $index 0]
-		set end [expr {[lindex $index 1] + 1}]
-		.editor.text tag add comment "1.0 + $start chars" "1.0 + $end chars"
-	}
-}
-
+#set ::scanner_regexp(comment) "(?:^|\n|;)(?:\[ \t\]*)(#\[^\n\]*)(?:\n)"
+#set ::scanner_regexp(comment) "(?:^|\n|;)(?:\[ \t\]*)(#\[^\n\]*)(?:\n|$)"	;# Improved to include comment at EOF
+#set ::scanner_regexp(comment) "(?:^|\n|;)(?:\[ \t\]*)(#(?:\\\\\n|\[^\n\])*?)(?:\n|$)"	;# Improved to extend comments across backslash-continued linebreaks.
+set ::scanner_regexp(comment) "(?:^|\n|;|\[)(?:\[ \t\]*)(#(?:\\\\\n|\[^\n\])*?)(?:\n|$)"	;# Improved to include comments after "["
 
 
 # Strings:
 
 # Well, everything is a string in Tcl, but strings allowing substitutions are written in double-quotes.
-# TODO: handle backslashes and things.
-proc tag_strings {} {
-	clear_tag string
-	foreach index [regexp -all -indices -inline "\"\[^\"\]*\"" [get_all]] {
-		set start [lindex $index 0]
-		set end [expr {[lindex $index 1] + 1}]
-		.editor.text tag add string "1.0 + $start chars" "1.0 + $end chars"
-	}
-}
+# TODO: handle backslashes and things.  Strings can span lines.
+# TODO: don't match strings inside comments.
+set ::scanner_regexp(string) "\"\[^\"\]*\""
 
 
-# Again, Tcl doesn't really have keywords, but certain commands are predefined and standard:
-proc tag_keywords {} {
-	clear_tag keyword
-	foreach index [regexp -all -indices -inline {eval|exec|exit|expr|foreach|package|proc|puts|require|set} [get_all]] {
-		set start [lindex $index 0]
-		set end [expr {[lindex $index 1] + 1}]
-		.editor.text tag add keyword "1.0 + $start chars" "1.0 + $end chars"
-	}
-}
+# Again, Tcl doesn't really have keywords, but certain commands and their arguments are predefined and standard:
+# join [lsort [info commands]] |
+set ::scanner_regexp(keyword) {LoadBLT|after|append|apply|array|auto_execok|auto_import|auto_load|auto_load_index|auto_mkindex|auto_mkindex_old|auto_qualify|auto_reset|bell|binary|bind|bindtags|break|button|canvas|capture|case|catch|cd|chan|checkbutton|clipboard|clock|close|concat|continue|destroy|dict|encoding|entry|eof|error|eval|event|exec|exit|expr|fblocked|fconfigure|fcopy|file|fileevent|flush|focus|font|for|foreach|format|frame|gets|glob|global|grab|grid|histogram|history|if|image|incr|info|interp|join|label|labelframe|lappend|lassign|lcount|lindex|linsert|list|listbox|llength|load|lower|lrange|lrepeat|lreplace|lreverse|lsearch|lset|lsort|map|match|mean|menu|menubutton|message|namespace|open|option|pack|package|panedwindow|pid|pkg_mkIndex|place|prettify|proc|puts|pwd|radiobutton|raise|rate|rate_histogram|read|regexp|regsub|rename|repeat|return|scale|scan|scrollbar|seek|selection|send|set|shuffle|sleep|slurp|socket|source|spinbox|split|string|subst|sum|sum2|switch|system|tclLog|tclPkgSetup|tclPkgUnknown|tcl_findLibrary|tclgrep|tell|text|time|time_histogram|tk|tk_chooseColor|tk_chooseDirectory|tk_getOpenFile|tk_getSaveFile|tk_menuSetFocus|tk_messageBox|tk_popup|tk_textCopy|tk_textCut|tk_textPaste|tkwait|toplevel|trace|unknown|unload|unset|update|uplevel|upvar|variable|vwait|while|winfo|wm}
+
+
 
 # Variable references.  Again, we can't catch all of them ("set var" is the same as "$var", for instance).
 #proc tag_variables {} {
 #	clear_tag variable
 #}
 
-# Probably quite important in Tcl are the brackets.  Perhaps bold for these?
-# TODO
+# Probably quite important in Tcl are the brackets.  Tcl has extremely minimal syntax; this is about it.  Perhaps bold for these?
+# TODO: Remember also that a backslash can continue a line (i.e. "\\\n" does not terminate a command.)
+# NOTE: remember that braces inside comments ARE significant because they are actually part of the core syntax.
+set ::scanner_regexp(symbol) {\[|\]|\{|\}|;|$}
 
--- scanning.tcl
+++ scanning.tcl
@@ -7,9 +7,11 @@
 # Some general setup first: apply colouring/highlighting preferences:
 .editor.text tag configure comment -foreground $::comment_foreground_colour
 .editor.text tag configure string -foreground $::string_foreground_colour
-.editor.text tag configure keyword -foreground $::keyword_foreground_colour	-font $::keyword_font;# bold? that's a font attribute.
+.editor.text tag configure keyword -foreground $::keyword_foreground_colour -font $::keyword_font;# bold? that's a font attribute.
+.editor.text tag configure symbol -foreground $::keyword_foreground_colour -font $::keyword_font
 
 # Now, try to get the tag priority right:
+.editor.text tag raise keyword symbol
 .editor.text tag raise string keyword
 .editor.text tag raise comment string
 
@@ -19,17 +21,46 @@ proc clear_tag {tag_name} {
 	.editor.text tag remove $tag_name 1.0 end
 }
 
+# Generic "scan text for regexp X and tag as Y".
+# This will be called repeatedly by the "tag_all" procedure.
+# You can use non-capturing groups in the regexp for a lexical element, e.g. "(?:LEADING_CONTEXT)(MATCH_THIS)(?:TRAILING_CONTEXT)".  This procedure will query the number of groups (not the non-matching ones) and make sure it ignores the indexes returned for the entire range (if applicable).
+proc tag {regexp tag_name} {
+	clear_tag $tag_name
+	
+	# The correct stride of this foreach loop depends on the number of match groups in the regexp.  Find out how many submatches the regexp involves, and set up the loop variable list accordingly:
+	if {[lindex [regexp -about -all -indices -inline $regexp {}] 0] == 0} {
+		set var_list {index}
+	} else {
+		set var_list {ignore index}
+	}
+	foreach $var_list [regexp -all -indices -inline $regexp [get_all]] {
+		set start [lindex $index 0]
+		set end [expr {[lindex $index 1] + 1}]
+		.editor.text tag add $tag_name "1.0 + $start chars" "1.0 + $end chars"
+	}
+	catch {unset index}
+	catch {unset ignore}
+}
+
+
 # Maybe the main scanner would look something like:
+# A good way to encode the symbols and REs for a language scanner might be to use an (associative) array, perhaps named ::scanner_regexp.  It would have an element for each generic lexical element that could be present in a language (indeed, maybe missing some).  Each time you load a language scanner, it'll can just replace the other stuff (but don't forget to use unset/array unset to avoid stuff hanging around!).
+# e.g. set ::scanner_regexp(keyword) {eval|exec|exit|expr|foreach|package|proc|puts|require|set|...}
 proc tag_all {} {
-	# set text [get_all]
-	foreach lexical_element {comments literals strings numbers keywords operators symbols variables} {
-		catch {tag_$lexical_element}	;# Some might not exist
+	# set text [get_all]	;# TODO: enable this and only fetch once!
+	foreach lexical_element {comment literal string number keyword operator symbol variable} {
+		# The regexp context is optional; dealing with that is surprisingly annoying (::scanner_regexp_context($lexical_element) might not exist).
+		catch {tag $::scanner_regexp($lexical_element) $lexical_element}	;# catch, to ignore any that don't exist
 	}
 }
 
 # Refresh the highlighting periodically.
 every 500 tag_all
 # TODO: How do we cancel that, if we ever want/need to turn the live highlighting off?
+
+
+
+
 
 
 
