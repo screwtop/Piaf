8692fa7b13cbee5af98813c8ab89435883ae875e
Chris Edwards <cedwards.github@cedwards.geek.nz>
2014-02-02 13:00:42 +1300

    Make command history in the console work.

--- console.tcl
+++ console.tcl
@@ -4,13 +4,26 @@
 # Should also be possible to show/hide.
 # TODO: make it work with commands containing quoted/braced spaces!
 
+set ::console_history_index 0
+
 toplevel .console
 wm title .console "[tk appname] console"
 pack [entry .console.entry -textvar command -font $::font] -fill x -side top
 bind .console.entry <Key-Return> {execute %W}
 bind .console.entry <Key-Up> {
-	set command [lindex $::console_history end]
-	# TODO: proper navigation of history, not just last command!
+	incr ::console_history_index
+	if {$::console_history_index > [llength $::console_history]} {set ::console_history_index [llength $::console_history]}
+	set command [lindex $::console_history end-$::console_history_index]
+	.console.entry icursor end
+}
+bind .console.entry <Key-Down> {
+	incr ::console_history_index -1
+	if {$::console_history_index < 0} {
+		set ::console_history_index -1
+		set command ""
+	} else {
+		set command [lindex $::console_history end-$::console_history_index]
+	}
 	.console.entry icursor end
 }
 pack [text .console.output -wrap word] -fill both -expand 1
@@ -20,11 +33,13 @@ set ::console_history [list]
 
 proc execute {w} {
 	global command
+	if {$command == ""} {return}
 	.console.output insert end "% $command\n"	;# TODO: tag showing it's user input
 	catch {uplevel 1 $command} result
 	.console.output insert end $result\n
 	.console.output see end
 	lappend ::console_history $command
+	set ::console_history_index -1
 	set command ""
 	unset result
 }
@@ -36,13 +51,4 @@ focus .console.entry
 
 wm withdraw .console	;# Hidden by default
 wm protocol .console WM_DELETE_WINDOW {wm withdraw .console}	;# Hide rather than close when "closed".
-
-
-
-
-
-
-
-
-
 
